73c14da037a56abfde06d6c4320bd09d0187d468
Irfan <fadillairfanaufa@gmail.com>
2026-09-14 15:53:36 +0700

    feat(user service): make build workflow

--- services/user-service/main.go
+++ services/user-service/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/wilismadiun/tembusptn/services/user-service/commons/database"
+	"github.com/wilismadiun/tembusptn/services/user-service/src/Infrastructures/driver"
+)
+
+func main() {
+	router := gin.Default()
+
+	database.ConnectDatabase()
+
+	driver.Router(router, database.DB)
+
+	router.Run(":3000")
+}
