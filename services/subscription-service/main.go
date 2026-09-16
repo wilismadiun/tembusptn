79647be8f0f93deccc40ba53734448a40e75bbe8
Irfan <fadillairfanaufa@gmail.com>
2026-09-16 16:12:56 +0700

    feat(subscription service): add create subscription endpoint

--- services/subscription-service/main.go
+++ services/subscription-service/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"subscription-service/commons/database"
+	"subscription-service/src/Infrastructures/driver"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	database.ConnectDatabase()
+
+	driver.Router(router, database.DB)
+
+	router.Run(":3001")
+}
